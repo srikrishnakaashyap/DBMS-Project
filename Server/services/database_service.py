f2344e87dd42e42c1ded76c312d2b3b7f9298362
Kaashyap <mskrishna.kaashyap@gmail.com>
2022-04-27 17:03:57 -0400

    Changes

--- Server/services/database_service.py
+++ Server/services/database_service.py
@@ -4,13 +4,16 @@ import mysql.connector
 from flask import jsonify
 
 import time
+
+
 class DatabaseService:
 
     MySqlConnection = None
     RedShiftConnection = None
 
-
     def execute_redshift_query(self, query):
+
+        print("EXECUTING REDSHIFT")
 
         conn = DatabaseService.get_connection("REDSHIFT")
         cursor = conn.cursor()
@@ -34,8 +37,6 @@ class DatabaseService:
 
         return jsonify({"Header": cols, "Response": answer, "ElapsedTime": t*1000, "Status": 200})
 
-
-    
     def execute_mysql_query(self, query):
 
         conn = DatabaseService.get_connection("MYSQL")
@@ -62,54 +63,56 @@ class DatabaseService:
 
         return jsonify({"Header": cols, "Response": answer, "ElapsedTime": t*1000, "Status": 200})
 
-
     @staticmethod
     def get_credentials(database):
 
         if database == "MYSQL":
 
-            ENDPOINT="rutgersdatabase.c3evtogdubqe.us-east-1.rds.amazonaws.com"
-            PORT=3306
-            USER="RutgersDataBase"
-            PASSWORD="Rutgers456"
-            REGION="us-east-1a"
-            DBNAME="Instacart"
+            ENDPOINT = "rutgersdatabase.c3evtogdubqe.us-east-1.rds.amazonaws.com"
+            PORT = 3306
+            USER = "RutgersDataBase"
+            PASSWORD = "Rutgers456"
+            REGION = "us-east-1a"
+            DBNAME = "Instacart"
 
-            return ENDPOINT,PORT,USER,PASSWORD,REGION,DBNAME
+            return ENDPOINT, PORT, USER, PASSWORD, REGION, DBNAME
 
         elif database == "REDSHIFT":
-            ENDPOINT="rutgersdbmsproject.c3h5sc0bvfmu.us-east-1.redshift.amazonaws.com"
-            PORT=5439
-            USER="rutgersdatabase"
-            PASSWORD="Rutgers456"
-            REGION="us-east-1"
-            DBNAME="instacart"
-            return ENDPOINT,PORT,USER,PASSWORD,REGION,DBNAME
+            ENDPOINT = "rutgersdbmsproject.c3h5sc0bvfmu.us-east-1.redshift.amazonaws.com"
+            PORT = 5439
+            USER = "rutgersdatabase"
+            PASSWORD = "Rutgers456"
+            REGION = "us-east-1"
+            DBNAME = "instacart"
+            return ENDPOINT, PORT, USER, PASSWORD, REGION, DBNAME
+            # return ENDPOINT, PORT, USER, PASSWORD, REGION, DBNAME
 
     @staticmethod
     def create_connection():
 
         try:
-            ENDPOINT,PORT,USER,PASSWORD,REGION,DBNAME = DatabaseService.get_credentials("REDSHIFT")
+            ENDPOINT, PORT, USER, PASSWORD, REGION, DBNAME = DatabaseService.get_credentials(
+                "REDSHIFT")
             conn = psycopg2.connect(
-            host=ENDPOINT,
-            port=PORT,
-            user=USER,
-            database=DBNAME,
-            password=PASSWORD)
+                host=ENDPOINT,
+                port=PORT,
+                user=USER,
+                database=DBNAME,
+                password=PASSWORD)
             DatabaseService.RedShiftConnection = conn
 
         except Exception as err:
             print(err.code, err)
-        
+
         try:
-            ENDPOINT,PORT,USER,PASSWORD,REGION,DBNAME = DatabaseService.get_credentials("MYSQL")
+            ENDPOINT, PORT, USER, PASSWORD, REGION, DBNAME = DatabaseService.get_credentials(
+                "MYSQL")
             conn = mysql.connector.connect(
-            host=ENDPOINT,
-            port=PORT,
-            user=USER,
-            database=DBNAME,
-            password=PASSWORD)
+                host=ENDPOINT,
+                port=PORT,
+                user=USER,
+                database=DBNAME,
+                password=PASSWORD)
             DatabaseService.MySqlConnection = conn
 
         except Exception as err:
@@ -117,7 +120,7 @@ class DatabaseService:
 
     @staticmethod
     def get_connection(database):
-        
+
         if(database == "MYSQL"):
             if DatabaseService.MySqlConnection == None:
                 DatabaseService.create_connection()
@@ -127,4 +130,3 @@ class DatabaseService:
                 DatabaseService.create_connection()
             conn = DatabaseService.RedShiftConnection
         return conn
-
